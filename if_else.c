df6e3ef17773323dc15f21b53cd06035658ef62f
Manan patel <mananpatel5348@gmail.com>
2026-07-25 17:49:47 +0530

    Add if-else example program in C to compare two integers
    This commit introduces a new C program that demonstrates the use of if-else statements to compare two integers. The program initializes two variables, `a` and `b`, and prints which one is larger.

---
+++ if_else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=10 , b = 20;
+    if(a>b)
+    {
+        printf("a is bigger number");
+    }
+    else
+    {
+        printf("b is bigger number");
+    }
+    return 0;
+}
